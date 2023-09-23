c2d9ab4c637976f7206f32c19257ce7004d1e573
benos <benantonn@gmail.com>
2023-09-22 19:58:48 -0500

    add spring project

---
+++ src/main/java/com/benosnews/benosnewsjavaapi/BenosNewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.benosnews.benosnewsjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenosNewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenosNewsJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benosnews/benosnewsjavaapi/BenosNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benosnews.benosnewsjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenosNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
