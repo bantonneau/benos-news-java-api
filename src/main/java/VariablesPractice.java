65a6597ad259873f542932c8a10a143a0255fdc4
benos <benantonn@gmail.com>
2023-09-22 20:31:53 -0500

    variable practice

--- src/main/java/VariablesPractice.java
+++ src/main/java/VariablesPractice.java
@@ -1,2 +1,8 @@
 public class VariablesPractice {
+    public static void main(String[] args) {
+        int A = 25;
+        double B = 30.56;
+        double C = A + B;
+        System.out.println(C);
+    }
 }
