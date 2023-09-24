50951a189f4f08bc36dd0568b91891fb8b7c99e3
benos <benantonn@gmail.com>
2023-09-24 11:41:30 -0500

    reset

--- src/main/java/VariablesPractice.java
+++
@@ -1,8 +0,0 @@
-public class VariablesPractice {
-    public static void main(String[] args) {
-        int A = 25;
-        double B = 30.56;
-        double C = A + B;
-        System.out.println(C);
-    }
-}
