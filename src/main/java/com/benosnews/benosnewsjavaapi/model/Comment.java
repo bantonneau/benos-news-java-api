3d2e80ee72e1bfefd4a95bf0228b095b93cdd367
benos <benantonn@gmail.com>
2023-10-23 18:27:01 -0500

    instantiate comment class

---
+++ src/main/java/com/benosnews/benosnewsjavaapi/model/Comment.java
@@ -0,0 +1,10 @@
+package com.benosnews.benosnewsjavaapi.model;
+
+public class Comment {
+    private Integer id;
+    private String commentText;
+    private Integer userId;
+    private Integer postId;
+    
+
+}
