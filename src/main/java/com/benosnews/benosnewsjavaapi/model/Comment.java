5ca210200413a7bb2ecd13f437d42c7c73280485
benos <benantonn@gmail.com>
2023-10-24 18:12:03 -0500

    add comment variables and annotations

--- src/main/java/com/benosnews/benosnewsjavaapi/model/Comment.java
+++ src/main/java/com/benosnews/benosnewsjavaapi/model/Comment.java
@@ -1,10 +1,26 @@
 package com.benosnews.benosnewsjavaapi.model;
 
-public class Comment {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "comment")
+public class Comment implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String commentText;
     private Integer userId;
     private Integer postId;
+
+
+
+
+
 
 
 }
