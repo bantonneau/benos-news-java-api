19e2263693554f9c6772a9d2abb2f681983630d7
benos <benantonn@gmail.com>
2023-10-28 14:44:15 -0500

    create vote class, fix errors

--- src/main/java/com/benosnews/benosnewsjavaapi/model/User.java
+++ src/main/java/com/benosnews/benosnewsjavaapi/model/User.java
@@ -52,6 +52,14 @@ public class User implements Serializable {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -59,30 +67,39 @@ public class User implements Serializable {
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public boolean isLoggedIn() {
         return loggedIn;
     }
+
     public void setLoggedIn(boolean loggedIn) {
         this.loggedIn = loggedIn;
     }
+
     public List<Post> getPosts() {
         return posts;
     }
+
     public void setPosts(List<Post> posts) {
         this.posts = posts;
     }
+
     public List<Vote> getVotes() {
         return votes;
     }
+
     public void setVotes(List<Vote> votes) {
         this.votes = votes;
     }
+
     public List<Comment> getComments() {
         return comments;
     }
+
     public void setComments(List<Comment> comments) {
         this.comments = comments;
     }
--- src/main/java/com/benosnews/benosnewsjavaapi/model/Vote.java
+++ src/main/java/com/benosnews/benosnewsjavaapi/model/Vote.java
@@ -1,6 +1,9 @@
 package com.benosnews.benosnewsjavaapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -8,21 +11,24 @@ import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Table(name = "comment")
-public class Comment implements Serializable {
+@Table(name = "vote")
+public class Vote implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private String commentText;
     private Integer userId;
     private Integer postId;
 
-    public Comment() {
+    public Vote() {
     }
 
-    public Comment(Integer id, String commentText, Integer userId, Integer postId) {
+    public Vote(Integer id, Integer userId, Integer postId) {
         this.id = id;
-        this.commentText = commentText;
+        this.userId = userId;
+        this.postId = postId;
+    }
+
+    public Vote(Integer userId, Integer postId) {
         this.userId = userId;
         this.postId = postId;
     }
@@ -33,14 +39,6 @@ public class Comment implements Serializable {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getCommentText() {
-        return commentText;
-    }
-
-    public void setCommentText(String commentText) {
-        this.commentText = commentText;
     }
 
     public Integer getUserId() {
@@ -63,20 +61,19 @@ public class Comment implements Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Comment comment = (Comment) o;
-        return Objects.equals(id, comment.id) && Objects.equals(commentText, comment.commentText) && Objects.equals(userId, comment.userId) && Objects.equals(postId, comment.postId);
+        Vote vote = (Vote) o;
+        return Objects.equals(id, vote.id) && Objects.equals(userId, vote.userId) && Objects.equals(postId, vote.postId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, commentText, userId, postId);
+        return Objects.hash(id, userId, postId);
     }
 
     @Override
     public String toString() {
-        return "Comment{" +
+        return "Vote{" +
                 "id=" + id +
-                ", commentText='" + commentText + '\'' +
                 ", userId=" + userId +
                 ", postId=" + postId +
                 '}';
